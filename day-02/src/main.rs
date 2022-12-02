411c243eedfa2d0499ceb14e2d9d86a7c9486af3
Kaio Silveira <silveira.kaio@icloud.com>
2022-12-02 10:54:54 +0000

    move RealStrategy out of the main lib file

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,25 +1,30 @@
+use day_02::strategies::rock_paper_scissors_real_strategy::*;
 use day_02::utils::{letter_to_suggested_result::*, letter_to_symbol::*};
-use day_02::*;
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("sample.txt").expect("should be able to read the input file");
     let contents: Vec<&str> = contents.lines().collect();
 
+    run_real_strategy(&contents);
+}
+
+fn run_real_strategy(contents: &Vec<&str>) {
     let mut total_score = 0;
+
     for line in contents {
         let round_info: Vec<&str> = line.split(" ").collect();
         let opponents_play = round_info
             .get(0)
             .expect("should be able to find the opponent's play");
 
-        let your_play = round_info
+        let suggested_result_letter = round_info
             .get(1)
-            .expect("should be able to find the user's plays");
+            .expect("should be able to find the suggested result letter");
 
         let round = RockPaperScissorsRealStrategy::build(
             letter_to_symbol(opponents_play),
-            letter_to_suggested_result(your_play),
+            letter_to_suggested_result(suggested_result_letter),
         );
 
         println!(
