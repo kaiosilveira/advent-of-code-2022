23b47e5e4610707a0107b005a75c514aa9cfba02
Kaio Silveira <silveira.kaio@icloud.com>
2022-12-04 11:29:58 +0000

    day-04: remove println at main

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use day_04::{create_ranges_from_pair_info, range_fully_contains_other, range_overlaps};
+use day_04::{create_task_ranges_from_pair_info, range_fully_contains_other, range_overlaps};
 
 fn main() {
     let contents = fs::read_to_string("sample.txt").expect("Should be able to read the file");
@@ -8,7 +8,7 @@ fn main() {
     let mut total_fully_contained = 0;
     let mut total_overlaps = 0;
     for line in contents.lines() {
-        let (r1, r2) = create_ranges_from_pair_info(line);
+        let (r1, r2) = create_task_ranges_from_pair_info(line);
         let overlaps = range_overlaps(&r1, &r2) || range_overlaps(&r2, &r1);
         let fully_contained =
             range_fully_contains_other(&r1, &r2) || range_fully_contains_other(&r2, &r1);
