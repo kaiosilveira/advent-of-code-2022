13fcb4456a178f0918d84b452b144ba35196aa0f
Kaio Silveira <silveira.kaio@icloud.com>
2022-12-05 13:49:12 +0000

    day-05: move utility functions out of main lib.rs file

--- day-05/src/lib.rs
+++ day-05/src/lib.rs
@@ -1,61 +1,20 @@
 mod crane_movers;
 mod stacks;
-
-use regex::Regex;
+mod utils;
 
 use crane_movers::{
-    commands::move_command::CraneMoverCommand,
     cranes::{crane_mover_9000::CraneMover9000, crane_mover_9001::CraneMover9001},
     strategies::move_crane_strategy::MoveCraneStrategy,
 };
 use stacks::crate_stack::CrateStack;
+use utils::input_parsing::{get_number_of_columns_from, process_input_lines};
 
-pub fn parse_crate_line(line: &str) -> Vec<String> {
-    line.chars()
-        .collect::<Vec<char>>()
-        .chunks(4)
-        .map(|c| c.iter().collect::<String>())
-        .map(|s| String::from(s.trim()))
-        .collect::<Vec<String>>()
+pub fn move_crates_one_by_one(input_lines: &Vec<&str>) -> String {
+    process_commands_and_return_the_topmost_item(&CraneMover9000::new(), input_lines)
 }
 
-pub fn parse_command_line(line: &str) -> Vec<usize> {
-    let mut cmd: Vec<usize> = vec![];
-
-    for cap in Regex::new(r"\w+\s\d+\s?").unwrap().captures_iter(line) {
-        let cmd_part: Vec<&str> = cap.get(0).unwrap().as_str().trim().split(" ").collect();
-        cmd.push(cmd_part.get(1).unwrap().parse::<usize>().unwrap());
-    }
-
-    cmd
-}
-
-pub fn process_input_lines(lines: &Vec<&str>) -> (Vec<Vec<String>>, Vec<CraneMoverCommand>) {
-    let mut item_rows: Vec<Vec<String>> = vec![];
-    let mut commands: Vec<CraneMoverCommand> = vec![];
-    for line in lines {
-        if line.contains("[") {
-            let line_data = parse_crate_line(line);
-            item_rows.push(line_data);
-        } else if line.contains("move") {
-            let cmd = parse_command_line(line);
-            let crane_quantity = *cmd.get(0).unwrap();
-            let origin_stack_position = *cmd.get(1).unwrap();
-            let target_stack_position = *cmd.get(2).unwrap();
-
-            commands.push(CraneMoverCommand::new(
-                crane_quantity,
-                origin_stack_position,
-                target_stack_position,
-            ));
-        }
-    }
-
-    (item_rows, commands)
-}
-
-pub fn get_number_of_columns_from(rows: &Vec<Vec<String>>) -> usize {
-    rows.iter().map(|r| r.len()).max().unwrap()
+pub fn move_multiple_crates_at_once(input_lines: &Vec<&str>) -> String {
+    process_commands_and_return_the_topmost_item(&CraneMover9001::new(), input_lines)
 }
 
 pub fn get_topmost_item_from_each_stack(stacks: &Vec<CrateStack>) -> String {
@@ -83,14 +42,6 @@ pub fn process_commands_and_return_the_topmost_item(
     }
 
     get_topmost_item_from_each_stack(&stacks)
-}
-
-pub fn move_crates_one_by_one(input_lines: &Vec<&str>) -> String {
-    process_commands_and_return_the_topmost_item(&CraneMover9000::new(), input_lines)
-}
-
-pub fn move_multiple_crates_at_once(input_lines: &Vec<&str>) -> String {
-    process_commands_and_return_the_topmost_item(&CraneMover9001::new(), input_lines)
 }
 
 pub fn print_stacks(stacks: &Vec<Vec<String>>) {
@@ -143,35 +94,5 @@ mod tests {
 
         let r = move_multiple_crates_at_once(&contents);
         assert_eq!("MCD", r);
-    }
-
-    mod parse_crate_line_tests {
-        use super::*;
-
-        #[test]
-        fn should_parse_a_crate_line() {
-            let line = "[Z] [M] [P]";
-
-            let result = parse_crate_line(&line);
-
-            assert_eq!("[Z]", result.get(0).unwrap());
-            assert_eq!("[M]", result.get(1).unwrap());
-            assert_eq!("[P]", result.get(2).unwrap());
-        }
-    }
-
-    mod parse_command_line_tests {
-        use super::*;
-
-        #[test]
-        fn should_parse_a_command_line() {
-            let line = "move 1 from 2 to 1";
-
-            let result = parse_command_line(&line);
-
-            assert_eq!(1, *result.get(0).unwrap());
-            assert_eq!(2, *result.get(1).unwrap());
-            assert_eq!(1, *result.get(2).unwrap());
-        }
     }
 }
---
+++ day-05/src/utils/input_parsing.rs
@@ -0,0 +1,86 @@
+use regex::Regex;
+
+use crate::crane_movers::commands::move_command::CraneMoverCommand;
+
+pub fn parse_crate_line(line: &str) -> Vec<String> {
+    line.chars()
+        .collect::<Vec<char>>()
+        .chunks(4)
+        .map(|c| c.iter().collect::<String>())
+        .map(|s| String::from(s.trim()))
+        .collect::<Vec<String>>()
+}
+
+pub fn parse_command_line(line: &str) -> Vec<usize> {
+    let mut cmd: Vec<usize> = vec![];
+
+    for cap in Regex::new(r"\w+\s\d+\s?").unwrap().captures_iter(line) {
+        let cmd_part: Vec<&str> = cap.get(0).unwrap().as_str().trim().split(" ").collect();
+        cmd.push(cmd_part.get(1).unwrap().parse::<usize>().unwrap());
+    }
+
+    cmd
+}
+
+pub fn process_input_lines(lines: &Vec<&str>) -> (Vec<Vec<String>>, Vec<CraneMoverCommand>) {
+    let mut item_rows: Vec<Vec<String>> = vec![];
+    let mut commands: Vec<CraneMoverCommand> = vec![];
+    for line in lines {
+        if line.contains("[") {
+            let line_data = parse_crate_line(line);
+            item_rows.push(line_data);
+        } else if line.contains("move") {
+            let cmd = parse_command_line(line);
+            let crane_quantity = *cmd.get(0).unwrap();
+            let origin_stack_position = *cmd.get(1).unwrap();
+            let target_stack_position = *cmd.get(2).unwrap();
+
+            commands.push(CraneMoverCommand::new(
+                crane_quantity,
+                origin_stack_position,
+                target_stack_position,
+            ));
+        }
+    }
+
+    (item_rows, commands)
+}
+
+pub fn get_number_of_columns_from(rows: &Vec<Vec<String>>) -> usize {
+    rows.iter().map(|r| r.len()).max().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod parse_crate_line_tests {
+        use super::*;
+
+        #[test]
+        fn should_parse_a_crate_line() {
+            let line = "[Z] [M] [P]";
+
+            let result = parse_crate_line(&line);
+
+            assert_eq!("[Z]", result.get(0).unwrap());
+            assert_eq!("[M]", result.get(1).unwrap());
+            assert_eq!("[P]", result.get(2).unwrap());
+        }
+    }
+
+    mod parse_command_line_tests {
+        use super::*;
+
+        #[test]
+        fn should_parse_a_command_line() {
+            let line = "move 1 from 2 to 1";
+
+            let result = parse_command_line(&line);
+
+            assert_eq!(1, *result.get(0).unwrap());
+            assert_eq!(2, *result.get(1).unwrap());
+            assert_eq!(1, *result.get(2).unwrap());
+        }
+    }
+}
