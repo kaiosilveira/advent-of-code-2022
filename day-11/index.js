d9f7c2d93784d5c9c5375895dca2731844f19f5d
Kaio Silveira <silveira.kaio@icloud.com>
2022-12-13 14:52:34 +0000

    day-11: general refactorings + update docs

--- day-11/index.js
+++ day-11/index.js
@@ -1,6 +1,6 @@
-const { partOne, partTwo } = require("./solution");
-const fs = require("fs");
-let input = fs.readFileSync("sample.txt").toString().split("\n");
+const { partOne, partTwo } = require('./solution');
+const fs = require('fs');
+let input = fs.readFileSync('sample.txt').toString().split('\n');
 
 console.log(`Part I: ${partOne(input)}`);
 console.log(`Part II: ${partTwo(input)}`);
